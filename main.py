a460e102317222692f1497c03b175f8aeb01ced0
Swapneil Basutkar <swapneil@simplrops.com>
2024-09-01 13:12:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
